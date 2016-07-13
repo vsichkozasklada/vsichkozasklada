d9ec30b03b95da82568cc310f95e30f6cc020e45
Ivaylo Gluhchev <jsl@abv.bg>
2016-07-13 23:33:53 +0300

    Added Drop down menu functionality and Added DateTimePicker

---
+++ Mitko_SL/Project.Web/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Project.Web.Controllers
+{
+    public class TestController : ApiController
+    {
+        // private ApplicationDbContext _context;
+        // public TestController()
+        //{
+        //    _context = new ApplicationDbContext();
+        //}
+
+        //[HttpPost]
+        //public IHttpActionResult Attend([FromBody]int gigId)
+        //{
+        //    var attendance = new Attendence()
+        //    {
+        //        GigId = gigId,
+        //        AttendeeId = User.Identity.GetUserId()
+        //    };
+        //    _context.Attendences.Add(attendance);
+        //    _context.SaveChanges();
+        //    return Ok();
+        //}
+    }
+}
---
+++ Mitko_SL/Project.Web/Scripts/Custom/navbarDropDown.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+    $("#ddlNavBar").click(function () {
+        $("#submenu1").attr("style", "visibility:visible");
+        $("#submenu2").attr("style", "display:none");
+    });
+
+    $("#liItem1").hover(function () {
+        $("#submenu1").attr("style", "visibility:visible");
+        $("#submenu2").attr("style", "display:none");
+    });
+    $("#liItem2").hover(function () {
+        $("#submenu1").attr("style", "display:none");
+        $("#submenu2").attr("style", "visibility:visible");
+
+    });
+});

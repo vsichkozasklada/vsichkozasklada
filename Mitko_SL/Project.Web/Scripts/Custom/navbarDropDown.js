6daf74ee7158ab9923a9f499472515129483737d
Ivaylo Gluhchev <jsl@abv.bg>
2016-07-24 22:40:09 +0300

    Landing page ddl modifications

--- Mitko_SL/Project.Web/Scripts/Custom/navbarDropDown.js
+++ Mitko_SL/Project.Web/Scripts/Custom/navbarDropDown.js
@@ -1,5 +1,14 @@
 ﻿$(document).ready(function () {
 
+    var moz = $.browser.mozilla();
+    var userBrowser = $.browser.browser();
+
+    if (moz == true) {
+        $("#span-chevron").addClass("glyphicon glyphicon-chevron-right").attr("style", "padding-left: 38px;");
+    } else {
+        $("#span-chevron").addClass("glyphicon glyphicon-chevron-right pull-right");
+    }
+
     $("#ddlNavBar").click(function () {
         $("#submenu1").attr("style", "visibility:visible");
         $("#submenu2").attr("style", "display:none");

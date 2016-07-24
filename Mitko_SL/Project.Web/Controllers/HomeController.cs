eb8af75900552eed9ccd199977a3eb4ecec7d593
Ivaylo Gluhchev <jsl@abv.bg>
2016-07-24 22:38:59 +0300

    Landing page modifications

--- Mitko_SL/Project.Web/Controllers/HomeController.cs
+++ Mitko_SL/Project.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Project.Data;
+using Project.Models;
 using Project.Web.Extensions;
+using Project.Web.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,7 +20,21 @@ namespace Project.Web.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            var blogEntries = Data.BlogEntry.All()
+                .OrderByDescending(b => b.PublishDate)
+                .Take(3);
+
+            IList<BlogEntryShortViewModel> modelList = new List<BlogEntryShortViewModel>();
+
+            foreach (var item in blogEntries)
+            {
+                var model = MapBlogEntryShortViewModel(item);
+                if (model != null)
+                {
+                    modelList.Add(model);
+                }
+            }
+            return View(modelList);
         }
 
         public ActionResult About()
@@ -38,14 +54,14 @@ namespace Project.Web.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Contact(Project.Models.ContactQuery model) 
+        public ActionResult Contact(Project.Models.ContactQuery model)
         {
             if (ModelState.IsValid)
             {
                 model.DateSubmitted = DateTime.Now;
                 Data.ContactQuery.Add(model);
                 Data.SaveChanges();
-                
+
                 this.AddNotification("Запитването е изпратено успешно", NotificationType.SUCCESS);
                 return RedirectToAction("Index");
             }
@@ -53,10 +69,26 @@ namespace Project.Web.Controllers
             return View(model);
         }
 
-        public ActionResult Clear(Project.Models.ContactQuery model) 
+        public ActionResult Clear(Project.Models.ContactQuery model)
         {
             ModelState.Clear();
             return RedirectToAction("Contact");
+        }
+
+        private BlogEntryShortViewModel MapBlogEntryShortViewModel(BlogEntry item)
+        {
+            BlogEntryShortViewModel model = new BlogEntryShortViewModel()
+            {
+                ID = item.ID,
+                Category = item.Category,
+                ExpiryDate = item.ExpiryDate,
+                PublishDate = item.PublishDate,
+                Title = item.Title,
+                TextContent = string.IsNullOrEmpty(item.TextContent) ? string.Empty
+                    : item.TextContent.Length < 51 ? item.TextContent
+                    : string.Format("{0}...", item.TextContent.Remove(50))
+            };
+            return model;
         }
     }
 }
